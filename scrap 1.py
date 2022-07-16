009ac311fb17dff97fa10b446223205ec15d57bc
7mrizqi <69248259+7mrizqi@users.noreply.github.com>
2022-07-16 20:09:41 +0700

    Add files via upload

---
+++ scrap 1.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+from csv import writer
+
+
+url = "https://www.pikiran-rakyat.com/"
+
+webs = requests.get(url)  
+soup = BeautifulSoup (webs.content, 'html.parser')
+
+with open('QTI-test.csv', 'w', encoding='utf8', newline='')as file:
+    write = writer(file)
+    header = ['Title']
+    write.writerow(header)
+
+    populer = soup.find_all('div',class_='most__item')
+    for p in populer:
+        Title = p.find('h2',class_='most__title').get_text()
+        res = [Title]
+        # print (hasil)
+        write.writerow(res)
